e4d9029333490f640ceb1e39fddcedd2c37a0849
Joaoc <joaom@iadb.org>
2020-04-06 17:16:21 -0400

    add prod cron runner

--- run_prod.sh
+++ run_prod.sh
@@ -1,3 +1,3 @@
 git checkout master;
-cd /home/joaom/projects/waze_coronavirus;
-/home/joaom/.conda/envs/norm_env/bin/python src/entrypoint.py single --slug=dev --dryrun &>> log.log
+cd /home/joaom/projects/waze_coronavirus_prod;
+/home/joaom/.conda/envs/norm_env/bin/python src/entrypoint.py single --slug=prod --force &>> log.log
