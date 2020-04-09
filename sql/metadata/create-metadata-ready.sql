66872d53bac7f3e95c94e51e95877f9e6777d0dc
Joaoc <joaom@iadb.org>
2020-04-09 11:20:18 -0400

    add prod cron and small fix

--- sql/metadata/create-metadata-ready.sql
+++ sql/metadata/create-metadata-ready.sql
@@ -8,7 +8,7 @@ select
 	c.idb_code,
 	c.waze_code,
 	c.country_name_idb_eng,
-    ST_POLYGON(r.region_shapefile_wkt) region_shapefile_binary
+    ST_POLYGON(replace(r.region_shapefile_wkt, '"', '')) region_shapefile_binary
 from {{ athena_database }}.{{ slug }}_metadata_country_waze_to_iso c
 join {{ athena_database }}.{{ slug }}_metadata_regions_metadata r
 on c.iso2_code = r.country_iso
