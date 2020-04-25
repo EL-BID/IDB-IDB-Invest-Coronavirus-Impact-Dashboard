4b0cfab879d96140d62b2853c8cc33633ddb271b
Joaoc <joaom@iadb.org>
2020-04-25 14:05:27 -0400

    take out bloked roads

--- sql/daily/create-daily-raw-table.sql
+++ sql/daily/create-daily-raw-table.sql
@@ -8,7 +8,7 @@ select cast(uuid as varchar) uuid,
 		city, length, line,
 		from_unixtime(retrievaltime/1000) retrievaltime
 from spd_sdv_waze_reprocessed.jams_ready
-where regexp_like(datetime, '20200308|20200309|20200310|20200311|20200312|20200313|20200314|20200315')
+where regexp_like(datetime, '20200309|20200310|20200311|20200312|20200313|20200314|20200315')
 and
 		{% for filter in initial_filters %}
 		{%- if loop.last -%}
