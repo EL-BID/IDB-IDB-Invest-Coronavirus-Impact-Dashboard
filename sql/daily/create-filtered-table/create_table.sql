f5662a514c6299433b5104937ec55c84710ef588
Joaoc <joaom@iadb.org>
2020-05-06 19:21:04 -0400

    fix column ordering

--- sql/daily/create-filtered-table/create_table.sql
+++ sql/daily/create-filtered-table/create_table.sql
@@ -1,9 +1,9 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS {{ athena_database }}.{{ slug }}_{{ raw_table }}_{{ name }} (
   `year` int,
   `month` int,
-  `dow` int , 
   `day` int,
   `hour` int,
+  `dow` int , 
   `line` string,
   `tci` double
   )
@@ -15,3 +15,4 @@ CREATE EXTERNAL TABLE IF NOT EXISTS {{ athena_database }}.{{ slug }}_{{ raw_tabl
 	 TBLPROPERTIES (
 	  'classification'='orc', 
 	  'compressionType'='zlib');
+
