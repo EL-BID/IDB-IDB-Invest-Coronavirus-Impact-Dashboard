22d150c00e64cb51d681591f975774d4e15d1bb0
Joaoc <joaom@iadb.org>
2020-05-06 14:07:53 -0400

    add timezone conversion to filtered daily table

--- sql/daily/create-filtered-table/fill_table.sql
+++ sql/daily/create-filtered-table/fill_table.sql
@@ -4,14 +4,17 @@ with (
 	  format='orc', orc_compression = 'ZLIB'
       ) as
 with t as (
+	with tz as (
+		select timezone 
+		from {{ athena_database }}.{{ slug }}_analysis_metadata_variation 
+		where region_slug = '{{ partition }}'
+	)
 	select 
 		length,
 		line,
-		year(retrievaltime) as year,
-		month(retrievaltime) as month,
-		day(retrievaltime) as day,
-		hour(retrievaltime) as hour,
-		day_of_week(retrievaltime) as dow,
+		date_parse(rpad(cast(at_timezone(
+			retrievaltime, (select timezone from tz)
+		) as varchar), 19, '000'), '%Y-%m-%d %k:%i:%s') retrievaltime,
 		date_parse(format_datetime(date_add('minute', 
 									cast(date_diff('minute',
 										timestamp '{{ reference_timestamp }}', retrievaltime) / {{ feed_frequency }} as bigint) * {{ feed_frequency }},
@@ -29,16 +32,21 @@ with t as (
 		st_polygon('{{ region_shapefile_wkt | replace('"', '') }}'),
 		st_line(line))
 	{%endif %}
-)
+		)
 select 
-	year, 
-	month, 
-	dow,
-	day, 
-	hour, 
+	year(retrievaltime) as year,
+	month(retrievaltime) as month,
+	day(retrievaltime) as day,
+	hour(retrievaltime) as hour,
+	day_of_week(retrievaltime) as dow,
 	line,
 	sum(length) as tci
 from t
 where n_row = 1
 group by
-	year, month, day, hour, line, dow
+	year(retrievaltime),
+	month(retrievaltime),
+	day(retrievaltime),
+	hour(retrievaltime),
+	day_of_week(retrievaltime),
+	line
