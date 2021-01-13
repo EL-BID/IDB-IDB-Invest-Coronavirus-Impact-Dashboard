02583620c6f6a828d91294fe5455bc7be6cfa879
Sonia <soniame@iadb.org>
2021-01-13 15:56:12 -0500

    run prod and chron settings in makefile

--- run_prod.sh
+++ run_prod.sh
@@ -1,3 +1,3 @@
 git checkout master;
-cd ~/projects/waze_coronavirus;
-~/.conda/envs/norm_env/bin/python src/entrypoint.py single --slug=prod --force --dependency_graph_path=configs/dependency-graph-prod.yaml --config_path=configs/config-prod.yaml &>> log.log
+cd ~/projects/IDB-IDB-Invest-Coronavirus-Impact-Dashboard;
+~/.conda/envs/IDB-IDB-Invest-Coronavirus-Impact-Dashboard/bin/python src/entrypoint.py single --slug=prod --force --dependency_graph_path=configs/dependency-graph-prod.yaml --config_path=configs/config-prod.yaml &>> log.log
