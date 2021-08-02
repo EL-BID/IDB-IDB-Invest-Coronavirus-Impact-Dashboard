2cefc4ffffb70304178b409ca37e94fbd6e18df1
Sonia <soniame@iadb.org>
2021-08-02 14:01:37 -0400

    update of distribution function

--- src/runners/split_polygon.py
+++ src/runners/split_polygon.py
@@ -7,14 +7,11 @@ import osmpy
 from shapely.geometry import box, Polygon, MultiPolygon, GeometryCollection, shape
 from shapely import wkt
 from shapely.ops import transform
-import shapely
-# from shapely.wkt import loads
+# import shapely
 
 ## FUNCTIONS
-
 def intersection_func(line, geometry):
-    line_wkt = wkt.loads(line)
-    result = geometry.intersection(line_wkt).is_empty == False
+    result = geometry.intersection(wkt.loads(line)).is_empty == False
     return(int(result))
 
 def threshold_density_func(geometry, threshold_value):
@@ -22,15 +19,19 @@ def threshold_density_func(geometry, threshold_value):
     
     print('Running')
     
-    times = [intersection_func(line, geometry) for x in df_lines.line_wkt]
-    intersection = sum(times)
-    total = len(times)
+    # Intersection of lines within square
+    times = [intersection_func(line, geometry) for line in df_lines.line_wkt]
+    total_lines = df_lines.count_lines
     
-    print(intersection)
-    print(total)
-    print(intersection/total)
+    # Total lines in square
+    inter = sum([times[x]*total_lines[x] for x in range(len(total))])
+    total = sum(total_lines)
     
-    return (intersection/total) < (threshold_value/total)
+    print(f"Intersection {intersection}")
+    print(f"Total lines {total}")
+    print(f"Proportion {intersection/total}")
+    
+    return (inter/total) < (threshold_value/total)
 
 
 ## POLIGONOS
@@ -48,13 +49,14 @@ cm = str(datetime.today().strftime("%Y%m%d%H%m%s"))
 print(cm)
 
 # Preparing geometry ----
-polygon = 'POLYGON ((-77.10205078124999 -13.004557745339769, -72.158203125 -13.004557745339769, -72.158203125 -8.90678000752024, -77.10205078124999 -8.90678000752024, -77.10205078124999 -13.004557745339769))'
+# - Latin america BID
+polygon = 'POLYGON((-129.454 37.238,-90.781 27.311,-67.117 20.333,-68.721 17.506,-23.765 -9.114,-65.601 -60.714,-126.421 -23.479,-129.454 37.238))'
 geometry = wkt.loads(polygon)
 
 # Running katana splits ----
 result = osmpy.core.katana(geometry, 
                            threshold_func = threshold_density_func, 
-                           threshold_value = 800, 
+                           threshold_value = 110000, 
                            count = 100)
 print(len(MultiPolygon(result).geoms))
 #print(MultiPolygon(result))
